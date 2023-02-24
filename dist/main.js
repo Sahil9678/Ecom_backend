6d5df78fb1a2d34ba43efbc6fed0e2dbbadc0848
sahil godwal <godwalsahil786@gmail.com>
2023-02-24 17:51:36 +0530

    seventh commit

--- dist/main.js
+++ dist/main.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+console.log("port", process.env.PORT);
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(process.env.PORT || 3000)
+    await app.listen(process.env.PORT)
         .then(() => {
-        console.log("successfully stared on port 3000");
+        console.log(`successfully stared on port,${process.env.PORT}`);
     })
         .catch((error) => {
         console.log(error);
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 declare const module: any;
-
+console.log("port", process.env.PORT)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000)
+  await app.listen(process.env.PORT)
     .then(() => {
-      console.log("successfully stared on port 3000");
+      console.log(`successfully stared on port,${process.env.PORT}`);
     })
     .catch((error) => {
       console.log(error);
