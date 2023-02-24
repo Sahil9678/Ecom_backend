3b61fccc15bdd44c6ba386a276406bce1a5e1566
sahil godwal <godwalsahil786@gmail.com>
2023-02-24 17:52:28 +0530

    eigth commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 declare const module: any;
-console.log("port", process.env.PORT)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT)
